58fe9b9f5b33a3128c0bb74caf031e89491989e0
Mattia Bottaro <mattiabottaro9@gmail.com>
2018-10-30 12:19:20 +0100

    Improved modularity to Candidate Elimination

--- 2 - Candidate Elimination/CandidateElimination.py
+++ 2 - Candidate Elimination/CandidateElimination.py
@@ -1,5 +1,5 @@
 from copy import deepcopy
-Any = '?'
+
 Yes = 'Y'
 No = 'N'
 
@@ -35,42 +35,33 @@ S = [ [[],[],[],[],[],[]] ]
 def hasMoreGeneral(h,A):
     for a in A:
         if a != h:
-            # # print(len(a),len(h))
-            # # print("Comparing ",a," with ",h,"\n")
             find = True
             for i in list(range(len(a))):
                 if (len(a[i]) <= len(h[i]) and (not (set(a[i]) >= set(h[i])))):
+                    # it means hypotesis a is not comparable to h
                     find = False
                     break
             if find:
-                # # print(a, " is more general than ",h)
                 return True
 
     return False
 
 # return True iff any hyp in A is more specific than h
 def hasMoreSpecific(h,A):
-    # print(A)
     for a in A:
         if a != h:
-            # # print(len(a),len(h))
-            ## print("Comparing ",a," with ",h)
             find = True
             for i in list(range(len(a))):
                 if (len(a[i]) >= len(h[i])) and (not (set(a[i]) <= set(h[i]))):
+                    # it means hypotesis a is not comparable to h
                     find = False
                     break
             if find:
-                # # print(a, " is more specific than ",h)
-                ## print("\n")
                 return True
-    ## print(a, " is NOT more specific than ",h)
-    ## print("\n")
     return False
 
 # we need to generalize h such that it's constraints are satisfied by d
 def computeMinimalGeneralization(h,d):
-    # print("generalizing ",h," to ",d)
     new_h = h.copy()
     for i in list(range(len(h))):
         if d[i] not in h[i]:
@@ -81,90 +72,79 @@ def computeMinimalGeneralization(h,d):
 
 # we need to specialize h such that it's constraints are satisfied by d
 def computeMinimalSpecialization(h,d):
-    # print("Min spec from ",h, " to ",d)
-    h_copy, h_aux, h1 = deepcopy(h), deepcopy(h), deepcopy(h) 
+    h_aux, h1 = deepcopy(h), deepcopy(h),
     specializations = []
-    for i in list(range(len(h_copy))):
-        # # print("constr is ",h_copy[i], ". d[i]: ",d[i])
-        if d[i] in h_copy[i]:
-            h_copy[i].remove(d[i])
-        h_aux[i] = h_copy[i]
+    for i in list(range(len(h))):
+        if d[i] in h[i]:
+            h[i].remove(d[i])
+        h_aux[i] = h[i]
         if hasMoreSpecific(h_aux, S):
             specializations.append(h_aux)
         h_aux = h1.copy()
-        ## print("final constr is ",h_copy[i],"\n")
-    
-    # print("sPEC ARE ",specializations)
+
     return specializations
 
 # are the h's constraints satisfied by d?
 def isConsistentForS(h,d):
-    ## print("Analyzing h: ",h," with d: ",d,". ")
     for i in list(range(len(h))):
-           # # print("comparing ",h[i]," with ",d[i])
             if d[i] not in h[i]:
                 return False
     return True
 
+# watchout: d is a NEGATIVE example
 def isConsistentForG(h,d):
     return not isConsistentForS(h,d)
 
 def removeHypTooGeneral(S):
-    ## print("starting S: ",S)
     for s1 in S:
         for s2 in S:
             if s1 != s2:
                 find = True
                 for i in list(range(len(s1))):
-                    # # print("Comparing ",s1[i]," with ",s2[i],"\n")
                     if (len(s1[i]) <= len(s2[i]) and (not (set(s1[i]) >= set(s2[i])))): 
+                        # it means hypotesis s1 is not comparable to s2
                         find = False
                         break
                 if find:
                     S.remove(s1)
-    ## print("final S: ",S)
     return S
 
 def removeHypTooSpecific(G):
-    ## print("starting G: ",G)
     for g1 in G:
         for g2 in G:
             if g1!= g2:
                 find = True
                 for i in list(range(len(g1))):
-                    if (len(g1[i]) >= len(g2[i])) and (not (set(g1[i]) <= set(g2[i]))):
+                    if (len(g1[i]) >= len(g2[i])) and (not (set(g1[i]) <= set(g2[i]))): 
+                        # it means hypotesis g1 is not comparable to g2
                         find = False
                         break
                 if find:
                     G.remove(g1)
-    ## print("final G: ",G)
     return G
 
+# watchout: d is a POSITIVE example
 def removeInconsistentFromG(d,G):
     for g in G:
-        ## print("is ", g, " consistent with ",d," ?")
-        for i in list(range(len(d))):
-            ## print("compare ",h[i]," and ",a[i])
-            if d[i] not in g[i]:
-                ## print("No")
+        for i in list(range(len(d))): # compare each constraint in d with its corresponding constraint in s
+            if d[i] not in g[i]:      # d is posivite (and also g), but d[i] it involves values not in g[i] => g is incosistent
                 G.remove(g)
                 break
     return G
 
+# watchout: d is a NEGATIVE example
 def removeInconsistentFromS(d,S):
     for s in S:
-        # # print("is ", s, " consistent with ",d," ?")
-        for i in list(range(len(d))):
-           # # print("comparing ",h[i]," with ",d[i])
-            if d[i] not in s[i]:
-                ## print("Yes")
-                break
-            S.remove(s)
+        for i in list(range(len(d))): # compare each constraint in d with its corresponding constraint in s
+            if d[i] not in s[i]:      # d is negative and constraint d[i] involves different values than s[i] => s is consistent
+                break                 # go to constraint d[i+1] and s[i+1]
+            S.remove(s)               # each constraint in d[i] involves values in s[i]. But d is negative => s is inconsistent
     return S
+
+# CANDIDATE ELIMINATION ALGORITHM
 
 print("Starting G ",G,"\nStarting S ",S)
 for d in DATA:
-    # # print("Iterating G:",G,"\nIterating S",S,"\n")
     if d[1] == Yes:
         print("\ntraining with positive example: ",d[0])
         G = removeInconsistentFromG(d[0],G)
@@ -173,10 +153,8 @@ for d in DATA:
             if not isConsistentForS(s,d[0]):
                 del S[i]
                 min_gen = computeMinimalGeneralization(s,d[0])
-                # # print("MinGen is: ",min_gen,"\n")
                 S.extend(min_gen)
                 S = removeHypTooGeneral(S)
-                # # print("S is ",S)
             i = i + 1
     else:
         print("\ntraining with negative example: ",d[0])        
